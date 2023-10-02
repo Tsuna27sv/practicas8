6c997e4144e59ccd5de0f268da4ed7d99a21a724
Kevin Benavides <131724598+Tsuna27sv@users.noreply.github.com>
2023-10-02 14:49:34 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtapellido = new System.Windows.Forms.TextBox();
+            this.txtdireccion = new System.Windows.Forms.TextBox();
+            this.dataGridViewdatos = new System.Windows.Forms.DataGridView();
+            this.btagregar = new System.Windows.Forms.Button();
+            this.bteditar = new System.Windows.Forms.Button();
+            this.bteliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewdatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mantenimiento de clientes UNIVO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(19, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Apellido:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Direccion:";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(130, 115);
+            this.txtid.Multiline = true;
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(168, 29);
+            this.txtid.TabIndex = 5;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(130, 174);
+            this.txtnombre.Multiline = true;
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(168, 29);
+            this.txtnombre.TabIndex = 6;
+            // 
+            // txtapellido
+            // 
+            this.txtapellido.Location = new System.Drawing.Point(130, 225);
+            this.txtapellido.Multiline = true;
+            this.txtapellido.Name = "txtapellido";
+            this.txtapellido.Size = new System.Drawing.Size(168, 29);
+            this.txtapellido.TabIndex = 7;
+            // 
+            // txtdireccion
+            // 
+            this.txtdireccion.Location = new System.Drawing.Point(130, 285);
+            this.txtdireccion.Multiline = true;
+            this.txtdireccion.Name = "txtdireccion";
+            this.txtdireccion.Size = new System.Drawing.Size(168, 29);
+            this.txtdireccion.TabIndex = 8;
+            // 
+            // dataGridViewdatos
+            // 
+            this.dataGridViewdatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewdatos.Location = new System.Drawing.Point(383, 120);
+            this.dataGridViewdatos.Name = "dataGridViewdatos";
+            this.dataGridViewdatos.ReadOnly = true;
+            this.dataGridViewdatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewdatos.Size = new System.Drawing.Size(285, 165);
+            this.dataGridViewdatos.TabIndex = 9;
+            // 
+            // btagregar
+            // 
+            this.btagregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btagregar.Location = new System.Drawing.Point(23, 389);
+            this.btagregar.Name = "btagregar";
+            this.btagregar.Size = new System.Drawing.Size(75, 23);
+            this.btagregar.TabIndex = 10;
+            this.btagregar.Text = "Agregar";
+            this.btagregar.UseVisualStyleBackColor = true;
+            this.btagregar.Click += new System.EventHandler(this.btagregar_Click);
+            // 
+            // bteditar
+            // 
+            this.bteditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bteditar.Location = new System.Drawing.Point(130, 389);
+            this.bteditar.Name = "bteditar";
+            this.bteditar.Size = new System.Drawing.Size(75, 23);
+            this.bteditar.TabIndex = 11;
+            this.bteditar.Text = "Editar";
+            this.bteditar.UseVisualStyleBackColor = true;
+            // 
+            // bteliminar
+            // 
+            this.bteliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bteliminar.Location = new System.Drawing.Point(244, 389);
+            this.bteliminar.Name = "bteliminar";
+            this.bteliminar.Size = new System.Drawing.Size(75, 23);
+            this.bteliminar.TabIndex = 12;
+            this.bteliminar.Text = "Eliminar";
+            this.bteliminar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bteliminar);
+            this.Controls.Add(this.bteditar);
+            this.Controls.Add(this.btagregar);
+            this.Controls.Add(this.dataGridViewdatos);
+            this.Controls.Add(this.txtdireccion);
+            this.Controls.Add(this.txtapellido);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewdatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtapellido;
+        private System.Windows.Forms.TextBox txtdireccion;
+        private System.Windows.Forms.DataGridView dataGridViewdatos;
+        private System.Windows.Forms.Button btagregar;
+        private System.Windows.Forms.Button bteditar;
+        private System.Windows.Forms.Button bteliminar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conexion = new SqlConnection("Data Source=ASUS;Initial Catalog=Empresa;integrated Security=true; TrustServerCertificate=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btagregar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+
+            string consulta = "INSERT INTO EMPLEADO VALUES" + "("+ txtid.Text + "','" + txtnombre.Text + "','" + txtapellido + "','"+ txtdireccion.Text + "')";
+
+            SqlCommand comando = new SqlCommand(consulta, conexion);
+
+
+
+            
+        }
+    }
+}
